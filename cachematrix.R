8b0360b445ac220c5bf3114374f4566c6c6e5df7
arjay314 <arjay314@users.noreply.github.com>
2014-07-27 09:14:02 -0700

    add code to implement matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix defines a set of functions that operate on a given square matrix
+##      and stores results
+##
+## cacheSolve returns a matrix inverse, the inverse is calculated when not in cache,
+##      otherwise a value is returned from the cache
+##
+## both functions are modifications of the makeVector and cachemean examples in the Assignment 2 doc
 
-## Write a short comment describing this function
 
+## makeCacheMatrix accepts a matrix as input and returns a list of functions that
+##      1 - set the value of the matrix
+##      2 - get the value of the matrix
+##      3 - set the value of the matrix inverse
+##      4 - get the value of the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        matinv <- NULL
+        set <- function(y) {
+                x <<- y
+                matinv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) matinv <<- inverse
+        getinv <- function() matinv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve accepts a  list of functions (returned by makeCacheMatrix), 
+##    and returns the matrix inverse
+## the inverse is calculated only when it is null (not yet calculated),
+##    otherwise the cached value of the inverse returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        matinv <- x$getinv()
+        if(!is.null(matinv)) {
+                message("getting cached data")
+                return(matinv)
+        }
+        data <- x$get()
+        matinv <- solve(data, ...)
+        x$setinv(matinv)
+        matinv
 }
+
